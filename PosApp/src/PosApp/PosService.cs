7399aabb27dfcfaff28790c1a159cdbb9d32622b
Liu Xia <lxconan@gmail.com>
2016-07-24 17:35:18 +0800

    Implement pos service.

--- PosApp/src/PosApp/PosService.cs
+++ PosApp/src/PosApp/PosService.cs
@@ -1,17 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PosApp
 {
     public class PosService
     {
+        readonly IProductRepository m_repository;
+
         public PosService(IProductRepository repository)
         {
+            m_repository = repository;
         }
 
         public Receipt GetReceipt(IList<BoughtProduct> boughtProducts)
         {
-            throw new NotImplementedException();
+            Validate(boughtProducts);
+            IList<ReceiptItem> receiptItems = MergeReceiptItems(boughtProducts);
+            return new Receipt(receiptItems);
+        }
+
+        IList<ReceiptItem> MergeReceiptItems(IList<BoughtProduct> boughtProducts)
+        {
+            string[] barcodes = boughtProducts.Select(bp => bp.Barcode).Distinct().ToArray();
+            Dictionary<string, Product> boughtProductSet = m_repository
+                .GetByBarcodes(barcodes)
+                .ToDictionary(p => p.Barcode, p => p);
+            return boughtProducts
+                .GroupBy(bp => bp.Barcode)
+                .Select(g => new ReceiptItem(boughtProductSet[g.Key], g.Sum(bp => bp.Amount)))
+                .ToArray();
+        }
+
+        void Validate(IList<BoughtProduct> boughtProducts)
+        {
+            if (boughtProducts == null) { throw new ArgumentNullException(nameof(boughtProducts)); }
+            if (boughtProducts.Any(bp => bp.Amount <= 0))
+            {
+                throw new ArgumentException(nameof(boughtProducts));
+            }
+
+            string[] uniqueBarcodes = boughtProducts.Select(bp => bp.Barcode).Distinct().ToArray();
+            if (m_repository.CountByBarcodes(uniqueBarcodes) != uniqueBarcodes.Length)
+            {
+                throw new ArgumentException("Some of the products cannot be found.");
+            }
         }
     }
 }
