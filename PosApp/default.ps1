952979e4185bfd51e959285ebffe9ccf4a3c651b
Liu Xia <lxconan@gmail.com>
2016-07-25 13:59:35 +0800

    Extract migration command.

--- PosApp/default.ps1
+++ PosApp/default.ps1
@@ -1,6 +1,14 @@
-task build {
-    exec { msbuild ".\posApp.sln" }
+function do-migration () {
     $connectionString = 'Data Source=.\SQLExpress;Initial Catalog=pos;Integrated Security=True'
     $migrationTarget = ".\src\Pos.Migration\bin\Debug\Pos.Migration.dll"
     iex ".\packages\FluentMigrator.1.6.2\tools\Migrate.exe /connection '$connectionString' /db SqlServer2014 /target '$migrationTarget'"
+}
+
+task build {
+    exec { msbuild ".\posApp.sln" }
+    do-migration    
+}
+
+task migration {
+    do-migration
 }
